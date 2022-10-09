ea002ec86604b81212f6f9e5793da47c7b1bbf04
Basam C. Serad <58251967+basamserad@users.noreply.github.com>
2022-10-09 09:59:55 -0700

    database connect and swagger setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'absdb',
+      entities: [],
+      synchronize: true,
+    }), 
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
